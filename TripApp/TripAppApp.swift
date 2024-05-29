6f65e3ea45b6a118a4c353b9cc37e847fff200ec
devnoom <archilmenabde@yahoo.com>
2024-05-29 14:20:18 +0400

    Initial Commit

---
+++ TripApp/TripAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripAppApp.swift
+//  TripApp
+//
+//  Created by MacBook Air on 29.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TripAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
